8f1209c06f7ad29f4178beea05a704b581b29692
rohitchauhan0 <beatfeelrc121@gmail.com>
2023-12-13 09:00:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,205 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaArrowLeft, FaPause, FaPlay } from "react-icons/fa";
+import Icons from "./Component/Icons";
+import { FaRegHeart } from "react-icons/fa";
+import { FaMusic } from "react-icons/fa";
+import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
+import images1 from "./kaam/images/poster- (1).jpg"
+import { SONGS } from "./Data/Songs";
+import { TbPlayerTrackNext, TbPlayerTrackNextFilled, TbPlayerTrackPrevFilled } from "react-icons/tb";
 
-function App() {
+
+
+const App = () => {
+
+
+  const [currentTrack, setCurrentTrack] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentImage, setcurrentImage] = useState(images1)
+  const [currentSongName, setcurrentSongName] = useState("song1")
+  const audioRef = useRef(null)
+  const [songDuration, setsongDuration] = useState(0)
+  const [songTime, setsongTime] = useState(0)
+  const [currentIndex, setcurrentIndex] = useState(0)
+
+
+
+
+  const handlePlayPause = (selectedSong) => {
+    if (selectedSong.song === currentTrack && isPlaying) {
+      setIsPlaying(false);
+    } else {
+      setCurrentTrack(selectedSong.song);
+      setIsPlaying(true);
+      setcurrentImage(selectedSong.image)
+      setcurrentSongName(selectedSong.title)
+      setcurrentIndex(selectedSong.id)
+    }
+  };
+
+  useEffect(() => {
+    isPlaying? (audioRef.current.play()) : (audioRef.current.pause());
+  }, [currentTrack, isPlaying])
+
+
+  useEffect(() => {
+      const handleSongDuration = ()=>{
+        setsongDuration(Math.floor(audioRef.current.duration))
+      }
+      const handleSongTime = ()=>{
+        setsongTime(Math.floor(audioRef.current.currentTime))
+      }
+      
+
+      audioRef.current.addEventListener('loadedmetadata', handleSongDuration);
+      audioRef.current.addEventListener('timeupdate', handleSongTime)
+      return () => {
+        audioRef.current.removeEventListener('loadedmetadata', handleSongDuration);
+      audioRef.current.removeEventListener('timeupdate', handleSongTime)
+
+      };
+
+  }, [])
+  // console.log(Math.floor(audioRef.current.duration))
+
+  const formatTime = (timeInSeconds)=>{
+    const minute = Math.floor(timeInSeconds/60)
+    const second = Math.floor(timeInSeconds % 60)
+    return `${minute}:${second}`
+  }
+
+  const handleTimeChange = (e)=>{
+    const newTime = e.target.value 
+    audioRef.current.currentTime = newTime
+    setsongTime(newTime)
+  }
+  
+  const handlePlayNext = ()=>{
+    setcurrentIndex(currentIndex+1)
+    if(currentIndex < 16){
+      const newSong = SONGS[currentIndex].song
+    const newSongName = SONGS[currentIndex].title
+    const newSongImage = SONGS[currentIndex].image
+    setCurrentTrack(newSong)
+    setcurrentSongName(newSongName)
+    setcurrentImage(newSongImage)
+    }
+    else{
+      setcurrentIndex(0)
+    }
+  }
+  const handlePlayprev = ()=>{
+
+      setcurrentIndex(currentIndex-1)
+    if(currentIndex > 1){
+      const newSong = SONGS[currentIndex-2].song
+      const newSongName = SONGS[currentIndex-2].title
+      const newSongImage = SONGS[currentIndex-2].image
+      setCurrentTrack(newSong)
+      setcurrentSongName(newSongName)
+      setcurrentImage(newSongImage)
+    }
+    else{
+     setcurrentIndex(17)
+    }
+
+    
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className=" w-screen h-screen bg flex items-center justify-center">
+      {/* music div  */}
+      <div className=" w-full lg:w-[60%] h-[80%]  px-6 lg:flex-row flex-col flex justify-between">
+        {/* left div  */}
+        <div className=" w-[40%] h-full bg2 rounded-2xl flex items-center justify-between flex-col -mt-8 p-6">
+          {/* upper part  */}
+          <div className=" w-full flex items-center justify-between text-white">
+            <Icons IconName={FaArrowLeft} />
+            <p>Playing music...</p>
+            <Icons IconName={GiHamburgerMenu} />
+          </div>
+          <div className=" flex flex-col items-center justify-center gap-3">
+            <img
+              src={currentImage}
+              alt=""
+              className=" w-48 rounded-full h-48 object-cover shadowWhite icon-bg animate-spin  "
+            />
+            <p className=" text-white text-xl">{currentSongName}</p>
+
+          </div>
+
+          <div>
+          <div className=" w-full flex justify-between">
+          <p></p>
+          <p className=" text-red-500">{`${formatTime(songDuration)}`}</p>
+
+          </div>
+            <input type="range" className=" w-80 custom-range" onChange={handleTimeChange}  style={{
+    background: `linear-gradient(to right, #eb9722 ${((songTime / songDuration) * 100)}%, gray 0)`
+  }} value={songTime} max={songDuration} />
+          </div>
+          <div className=" flex gap-5">
+              <div className=" text-2xl icon-bg flex items-center justify-center h-fit p-4 bg  text-white cursor-pointer rounded-full hover:text-orange-500">
+                <TbPlayerTrackPrevFilled  onClick={handlePlayprev} />
+              </div>
+            <div className=" icon-bg shadowWhite2 p-4 px-5 text-2xl text-orange-500 cursor-pointer rounded-full">
+              <button onClick={() => setIsPlaying(!isPlaying)}>
+                {
+                  isPlaying ? (<FaPause/>):(<FaPlay/>)
+                }
+              </button>
+            </div>
+            <div className=" text-2xl icon-bg flex items-center justify-center h-fit p-4  text-white bg cursor-pointer rounded-full hover:text-orange-500">
+              <TbPlayerTrackNextFilled   onClick={handlePlayNext} />
+            </div>
+          </div>
+        </div>
+
+        {/* right div  */}
+        <div className=" w-[40%] h-full bg2 rounded-2xl mt-8 pt-8 px-3  flex flex-col gap-10  ">
+     
+        <div className=" w-full flex justify-between items-center">
+            <Icons IconName={FaRegHeart} />
+            <div>
+              <img
+                src={currentImage}
+                alt=""
+                className=" w-48 rounded-full h-48 object-cover shadowWhite icon-bg animate-spin  "
+              />
+            </div>
+            <Icons IconName={FaMusic} />
+          </div>
+     <div className="flex flex-col gap-5 overflow-auto container">
+    <audio ref={audioRef} src={currentTrack}/>
+      {SONGS.map((data) => (
+        <div
+          className={`${currentTrack === data.song ?"w-full flex justify-between items-center p-2 bg-orange-400 rounded-full":"w-full flex justify-between items-center p-2 shadowWhite rounded-full"}`}
+          key={data.id}
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <img src={data.image} alt="" className="hidden" />
+            <p className="hidden">{data.song}</p>
+            <p className="text-white">{data.title}</p>
+          </div>
+          <button onClick={() => handlePlayPause(data)}>
+            {currentTrack === data.song && isPlaying ? (
+              <Icons IconName={FaPause} />
+            ) : (
+              <Icons IconName={FaPlay} />
+            )}
+          </button>
+        </div>
+      ))}
+    </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Data/Songs.js
@@ -0,0 +1,116 @@
+// To play the sound
+
+import image1 from "../kaam/images/poster- (1).jpg";
+import image2 from "../kaam/images/poster- (2).jpg";
+import image3 from "../kaam/images/poster- (3).jpg";
+import image4 from "../kaam/images/poster- (4).jpg";
+import image5 from "../kaam/images/poster- (5).jpg";
+import image6 from "../kaam/images/poster- (6).jpg";
+import image7 from "../kaam/images/poster- (7).jpg";
+import image8 from "../kaam/images/poster- (8).jpg";
+import image9 from "../kaam/images/poster- (9).jpg";
+// import image10 from "../kaam/kaam/images/poster- (10).jpg"
+// import image11 from "../kaam/kaam/images/poster- (11).jpg"
+// import image12 from "../kaam/kaam/images/poster- (12).jpg"
+// import image13 from "../kaam/kaam/images/poster- (12).jpg"
+// import image14 from "../kaam/kaam/images/poster- (14).jpg"
+// import image15 from "../kaam/kaam/images/poster- (15).jpg"
+
+export const SONGS = [
+  {
+    id: 1,
+    title: "Song 1",
+    image: require("../kaam/images/poster- (1).jpg"),
+    song: require("../kaam/songs/song- (1).mp3"),
+  },
+  {
+    id: 2,
+    title: "Song 2",
+    image: require("../kaam/images/poster- (2).jpg"),
+    song: require("../kaam/songs/song- (2).mp3"),
+  },
+  {
+    id: 3,
+    title: "Song 3",
+    image: require("../kaam/images/poster- (3).jpg"),
+    song: require("../kaam/songs/song- (3).mp3"),
+  },
+  {
+    id: 4,
+    title: "Song 4",
+    image: require("../kaam/images/poster- (4).jpg"),
+    song: require("../kaam/songs/song- (4).mp3"),
+  },
+  {
+    id: 5,
+    title: "Song 5",
+    image: require("../kaam/images/poster- (5).jpg"),
+    song: require("../kaam/songs/song- (5).mp3"),
+  },
+  {
+    id: 6,
+    title: "Song 6",
+    image: require("../kaam/images/poster- (6).jpg"),
+    song: require("../kaam/songs/song- (6).mp3"),
+  },
+  {
+    id: 7,
+    title: "Song 7",
+    image: require("../kaam/images/poster- (7).jpg"),
+    song: require("../kaam/songs/song- (7).mp3"),
+  },
+  {
+    id: 8,
+    title: "Song 8",
+    image: require("../kaam/images/poster- (8).jpg"),
+    song: require("../kaam/songs/song- (8).mp3"),
+  },
+  {
+    id: 9,
+    title: "Song 9",
+    image: require("../kaam/images/poster- (9).jpg"),
+    song: require("../kaam/songs/song- (9).mp3"),
+  },
+  {
+    id: 10,
+    title: "Song 10",
+    image: require("../kaam/images/poster- (10).jpg"),
+    song: require("../kaam/songs/song- (10).mp3"),
+  },
+  {
+    id: 11,
+    title: "Song 11",
+    image: require("../kaam/images/poster-(11).jpg"),
+    song: require("../kaam/songs/song-(11).mp3"),
+  },
+  {
+    id: 12,
+    title: "Song 12",
+    image: require("../kaam/images/poster-(12).jpg"),
+    song: require("../kaam/songs/song-(12).mp3"),
+  },
+  {
+    id: 13,
+    title: "Song 13",
+    image: require("../kaam/images/poster-(13).jpg"),
+    song: require("../kaam/songs/song-(13).mp3"),
+  },
+  {
+    id: 14,
+    title: "Song 14",
+    image: require("../kaam/images/poster-(14).jpg"),
+    song: require("../kaam/songs/song-(14).mp3"),
+  },
+  {
+    id: 15,
+    title: "Song 15",
+    image: require("../kaam/images/poster-(15).jpg"),
+    song: require("../kaam/songs/song-(15).mp3"),
+  },
+  {
+    id: 16,
+    title: "Satranga ",
+    image: require("../kaam/images/animal.jpg"),
+    song: require("../kaam/songs/satranga.mp3"),
+  },
+];
